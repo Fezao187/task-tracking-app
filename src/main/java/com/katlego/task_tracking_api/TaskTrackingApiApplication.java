9a44cedf11b29db666776d2ad42a03c0ea9808f7
Fezao187 <katlegomaredi9@gmail.com>
2026-02-13 23:51:59 +0200

    chore: initialise springboot application

---
+++ src/main/java/com/katlego/task_tracking_api/TaskTrackingApiApplication.java
@@ -0,0 +1,13 @@
+package com.katlego.task_tracking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackingApiApplication.class, args);
+    }
+
+}

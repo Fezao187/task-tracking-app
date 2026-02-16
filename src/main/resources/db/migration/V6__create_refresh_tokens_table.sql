75c656bc31137b3c799261c722dedec24f98f22d
Fezao187 <katlegomaredi9@gmail.com>
2026-02-16 14:55:42 +0200

    feat(db): create refresh token entity

--- src/main/resources/db/migration/V6__create_refresh_tokens_table.sql
+++ src/main/resources/db/migration/V6__create_refresh_tokens_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE refresh_tokens
 (
     id         BIGSERIAL PRIMARY KEY,
     email      VARCHAR(150) NOT NULL,
-    token      VARCHAR(255) NOT NULL UNIQUE,
+    token      VARCHAR(500) NOT NULL UNIQUE,
     created_at TIMESTAMP    NOT NULL
 );

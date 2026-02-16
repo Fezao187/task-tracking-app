e5e746493f0a9809fe939eaa22e4e1d89e75c718
Fezao187 <katlegomaredi9@gmail.com>
2026-02-16 14:59:09 +0200

    feat(db): create refresh token repository

---
+++ src/main/java/com/katlego/task_tracking_api/security/repository/RefreshTokenRepository.java
@@ -0,0 +1,20 @@
+package com.katlego.task_tracking_api.security.repository;
+
+import com.katlego.task_tracking_api.security.entity.RefreshToken;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByEmailAndToken(String email, String token);
+
+    @Transactional
+    void deleteByEmail(String email);
+
+    @Transactional
+    void deleteByEmailAndToken(String email, String token);
+}

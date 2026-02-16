9e2d562684f121a63e974b463bc9bea932c6e127
Fezao187 <katlegomaredi9@gmail.com>
2026-02-16 15:15:27 +0200

    feat(security): Add schedular to delete expired refresh tokens

--- src/main/java/com/katlego/task_tracking_api/TaskTrackingApiApplication.java
+++ src/main/java/com/katlego/task_tracking_api/TaskTrackingApiApplication.java
@@ -2,8 +2,10 @@ package com.katlego.task_tracking_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TaskTrackingApiApplication {
 
     public static void main(String[] args) {

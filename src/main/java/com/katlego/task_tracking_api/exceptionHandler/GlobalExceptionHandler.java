7d192e28f0f6cf54900fe0f47b92977acc738ece
Fezao187 <katlegomaredi9@gmail.com>
2026-02-16 20:03:35 +0200

    chore(auth): handle MethodArgumentNotValidException globally for @NotBlank

--- src/main/java/com/katlego/task_tracking_api/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/katlego/task_tracking_api/exceptionHandler/GlobalExceptionHandler.java
@@ -3,13 +3,16 @@ package com.katlego.task_tracking_api.exceptionHandler;
 import com.katlego.task_tracking_api.exception.ResourceAlreadyExistException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -21,6 +24,19 @@ public class GlobalExceptionHandler {
                 status.value(),
                 status,
                 LocalDateTime.now()
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        String errorMessages = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining("; "));
+        return new ResponseEntity<>(
+                createErrorResponse(errorMessages, HttpStatus.BAD_REQUEST),
+                HttpStatus.BAD_REQUEST
         );
     }
 

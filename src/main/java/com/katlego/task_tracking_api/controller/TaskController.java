5eea18d6a69ad0401c26c928a411e67c9799aba5
Fezao187 <katlegomaredi9@gmail.com>
2026-02-17 15:14:04 +0200

    feat(task): add endpoint for admins to create task

---
+++ src/main/java/com/katlego/task_tracking_api/controller/TaskController.java
@@ -0,0 +1,28 @@
+package com.katlego.task_tracking_api.controller;
+
+import com.katlego.task_tracking_api.dto.task.CreateTaskRequest;
+import com.katlego.task_tracking_api.dto.task.TaskResponse;
+import com.katlego.task_tracking_api.service.TaskService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/task")
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/create")
+    public ResponseEntity<TaskResponse> createTask(@RequestBody CreateTaskRequest request){
+        return new ResponseEntity<>(taskService.createTask(request), HttpStatus.CREATED);
+    }
+}

2ec54915d7eb370f8b647e1778f99358d279b9f1
Fezao187 <katlegomaredi9@gmail.com>
2026-02-19 10:45:47 +0200

    chore(docs): Add swagger descritions on User controller

--- src/main/java/com/katlego/task_tracking_api/controller/UserController.java
+++ src/main/java/com/katlego/task_tracking_api/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.katlego.task_tracking_api.controller;
 
+import com.katlego.task_tracking_api.docs.ApiDocs;
 import com.katlego.task_tracking_api.dto.user.AdminCreateUserRequest;
 import com.katlego.task_tracking_api.dto.user.AdminCreateUserResponse;
 import com.katlego.task_tracking_api.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user")
+@Tag(name = ApiDocs.UserApi.TAG)
 public class UserController {
     private final UserService userService;
 
@@ -22,6 +26,10 @@ public class UserController {
 
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/create")
+    @Operation(
+            summary = ApiDocs.UserApi.ADMIN_CREATE_USER_SUMMARY,
+            description = ApiDocs.UserApi.ADMIN_CREATE_USER_DESC
+    )
     public ResponseEntity<AdminCreateUserResponse> adminCreateUser(@RequestBody AdminCreateUserRequest request){
         return new ResponseEntity<>(userService.adminCreateUser(request), HttpStatus.CREATED);
     }
